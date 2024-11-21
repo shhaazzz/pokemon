60a133d60686e4c37393c16be50e6e359cbd8f0b
shahnawaz <shhaazzz@gmail.com>
2024-11-21 13:33:43 +0530

    task-3

--- packages/ui/components/List.tsx
+++ packages/ui/components/List.tsx
@@ -1,10 +1,11 @@
 // src/ui/List.tsx
 import React from "react";
+import "./List.css"; // Import CSS for styling
 
 interface ListProps {
   children: React.ReactNode;
 }
 
 export const List = ({ children }: ListProps) => {
-  return <ul>{children}</ul>;
+  return <ul className="list-grid">{children}</ul>;
 };
